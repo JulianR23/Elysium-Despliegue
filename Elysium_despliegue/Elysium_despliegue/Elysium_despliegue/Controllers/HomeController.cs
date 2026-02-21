3dafe1217efb115170fe400b97f8b01d94ac5ded
Nicolás Pérez Tabares <nicolastabares1010@gmail.com>
2026-02-21 15:38:58 -0500

    feat: mensaje1 resolución de conflictos

--- Elysium_despliegue/Elysium_despliegue/Elysium_despliegue/Controllers/HomeController.cs
+++ Elysium_despliegue/Elysium_despliegue/Elysium_despliegue/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Elysium_despliegue.Controllers
         [HttpGet]
         public string Get()
         {
-            return "Nuevo mensaje corregido";
+            return "Mensaje resolución de conflicto";
         }
     }
 }
